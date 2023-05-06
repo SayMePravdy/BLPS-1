d0f1e53e6d60cdd2d56f0822af12a9bfb037fedf
TotsSamuy <72630727+TotsSamuy@users.noreply.github.com>
2023-05-06 17:02:30 +0300

    add apigateway

--- src/main/java/itmo/blps/mommy/MommyApplication.java
+++ src/main/java/itmo/blps/mommy/MommyApplication.java
@@ -2,12 +2,14 @@ package itmo.blps.mommy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @Configuration
 @EnableJms
+@EnableDiscoveryClient
 public class MommyApplication {
 
     public static void main(String[] args) {
